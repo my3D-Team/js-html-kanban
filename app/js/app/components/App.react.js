1882548e4755c020cf72308ecc51f56e637ad830
DAMIE, Nathan <ndamie@LFR410479.corp.capgemini.com>
2015-08-21 17:06:01 +0200

    feat(zoom): change the zoom to btn instead of slider

--- app/js/app/components/App.react.js
+++ app/js/app/components/App.react.js
@@ -10,6 +10,7 @@ var React = require('react');
 
 var Header = require('../../Header.react.js');
 var Kanban = require('../../kanban/components/Kanban.react.js');
+var KanbanStore = require('../../kanban/stores/KanbanStore')
 
 var AppActions = require('../actions/AppActions')
 
@@ -21,25 +22,32 @@ var App = React.createClass({
 
                 <Header></Header>
                 <Kanban></Kanban>
-                <input className="zoom" type="range" min="60" max="200" defaultValue="100" onChange={this._manageZoom} step="10" />
-                <div className="html2canvas" onClick={this.generateCanvas}>
-                    <i className="fa fa-picture-o "></i>
+
+                <div className="zoom-in tools" onClick={this.zoomIn}>
+                    <i className="fa fa-plus"></i>
+                </div>
+                <div className="zoom-out tools" onClick={this.zoomOut}>
+                    <i className="fa fa-minus"></i>
+                </div>
+                <div className="html2canvas tools" onClick={this.generateCanvas}>
+                    <i className="fa fa-picture-o"></i>
                 </div>
             </div>
             );
     },
-    _manageZoom: function (e) {
-        AppActions.scale(e.currentTarget.value / 100);
+    zoomIn: function (e) {
+        AppActions.scale(KanbanStore.getScale() + 0.1);
+    },
+
+    zoomOut: function (e) {
+        AppActions.scale(KanbanStore.getScale() - 0.1);
     },
 
     generateCanvas: function () {
         html2canvas(document.body, {
             onrendered: function (canvas) {
                 var w = window.open();
-
                 w.document.body.appendChild(canvas);
-
-//                document.body.appendChild(canvas);
             }
         });
     }
