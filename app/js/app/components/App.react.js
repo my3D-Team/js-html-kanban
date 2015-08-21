e1f77ee77172c601105dd56640532ac58dd93912
DAMIE, Nathan <ndamie@LFR410479.corp.capgemini.com>
2015-08-21 16:50:11 +0200

    feat(html2canvas): add html2canvas btn export for test

--- app/js/app/components/App.react.js
+++ app/js/app/components/App.react.js
@@ -15,18 +15,36 @@ var AppActions = require('../actions/AppActions')
 
 var App = React.createClass({
 
-    render: function() {
+    render: function () {
         return (
-             <div>
+            <div>
+
                 <Header></Header>
-                <Kanban  ></Kanban>
+                <Kanban></Kanban>
                 <input className="zoom" type="range" min="60" max="200" defaultValue="100" onChange={this._manageZoom} step="10" />
+                <div className="html2canvas" onClick={this.generateCanvas}>
+                    <i className="fa fa-picture-o "></i>
+                </div>
             </div>
             );
     },
     _manageZoom: function (e) {
         AppActions.scale(e.currentTarget.value / 100);
+    },
+
+    generateCanvas: function () {
+        html2canvas(document.body, {
+            onrendered: function (canvas) {
+                var w = window.open();
+
+                w.document.body.appendChild(canvas);
+
+//                document.body.appendChild(canvas);
+            }
+        });
     }
+
+
 });
 
 module.exports = App;
