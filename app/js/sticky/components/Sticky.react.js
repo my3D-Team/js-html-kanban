ec54f1d4623ff6dba5e539d0f28bed807c5b2be8
Gabin Darras <gadarras@LFR409195.corp.capgemini.com>
2015-08-28 16:05:24 +0200

    feat(sticky) : simple style for sticky uncollapsed
    Signed-off-by: Gabin Darras <gadarras@LFR409195.corp.capgemini.com>

--- app/js/sticky/components/Sticky.react.js
+++ app/js/sticky/components/Sticky.react.js
@@ -8,8 +8,14 @@
 var React = require('react');
 var _ = require('lodash');
 
+// Mixins
 var StickyMixin = require('./mixins/StickyMixin');
+
+// Actions
 var StickyActions = require('../actions/StickyActions.js');
+
+// Stores
+var StickyStore = require('./../stores/StickyStore');
 
 var Sticky = React.createClass({
     mixins: [StickyMixin],
@@ -33,12 +39,25 @@ var Sticky = React.createClass({
     },
 
     render: function () {
-        var width = Constants.COLUMN.WIDTH - 2 * Constants.STICKY.PADDING,
-            title = this.getTitle();
+        var title = this.getTitle();
+        var sticky = {};
 
+        if (StickyStore.areStickiesCollapsed()) {
+            sticky = this.getCollapseSticky(title);
+        } else {
+            sticky = this.getUnCollapseSticky(title);
+        }
+
+        return sticky;
+
+    },
+
+    getCollapseSticky: function (title) {
+
+        var width = Constants.STICKY.WIDTH;
         var top = this.state.position.y ? this.state.position.y : this.props.y;
         var left = this.state.position.x ? this.state.position.x : this.props.x;
-        var height = this.state.dimension.height ? this.state.dimension.height : this.props.height;
+        var height = Constants.STICKY.HEIGHT.COLLAPSED;
         var zIndex = this.state.zIndex ? this.state.zIndex : this.props.zIndex;
         var css = {
             top: top + "px",
@@ -47,15 +66,59 @@ var Sticky = React.createClass({
             height: height + "px",
             zIndex: zIndex
         };
+
         return (
-            <div className={this.props.sticky.content.stickyCode + " sticky"} style={css} onTouchStart={this._onSelect}
+            <div className="sticky" style={css} onTouchStart={this._onSelect}
                  onTouchEnd={this._onDeselect} onMouseDown={this._onSelect} onMouseUp={this._onDeselect}>
-                <div className="sticky-title-wrapper">
+                <div className={this.props.sticky.content.stickyCode + " sticky-title-wrapper"}>
                     <div className="sticky-title">{title}</div>
                 </div>
             </div>
         );
     },
+
+    getUnCollapseSticky: function (title) {
+
+        var width = Constants.STICKY.WIDTH;
+        var top = this.state.position.y ? this.state.position.y : this.props.y;
+        var left = this.state.position.x ? this.state.position.x : this.props.x;
+        var height = Constants.STICKY.HEIGHT.NOT_COLLAPSED;
+        var zIndex = this.state.zIndex ? this.state.zIndex : this.props.zIndex;
+        var css = {
+            top: top + "px",
+            left: left + "px",
+            width: width + "px",
+            height: height + "px",
+            zIndex: zIndex
+        };
+
+        return (
+            <div className="sticky uncollapse" style={css} onTouchStart={this._onSelect}
+                 onTouchEnd={this._onDeselect} onMouseDown={this._onSelect} onMouseUp={this._onDeselect}>
+                <div className={this.props.sticky.content.stickyCode + " sticky-title-wrapper"}>
+                    <div className="sticky-title">{title}</div>
+                </div>
+                <div className="sticky-uncollapse-content-wrapper">
+                    <div className="sticky-uncollapse-content-cell">
+                        <div>3</div>
+                        <div>Est</div>
+                    </div>
+                    <div className="sticky-uncollapse-content-cell">
+                        <div>1</div>
+                        <div>Con</div>
+                    </div>
+                    <div className="sticky-uncollapse-content-cell">
+                        <div>2</div>
+                        <div>ETC</div>
+                    </div>
+                </div>
+                <div className="sticky-uncollapse-bottom-wrapper">
+                    <div className="sticky-uncollapse-bottom">titre</div>
+                </div>
+            </div>
+        );
+    },
+
 
     _onSelect: function (e) {
         this.state.hasMove = false;
@@ -64,7 +127,7 @@ var Sticky = React.createClass({
 
     _onDeselect: function (e) {
         if (!this.state.hasMove) {
-            this._toggleEditMode(e);
+            //this._toggleEditMode(e);
         }
         StickyActions.deselect(e, this);
     }
